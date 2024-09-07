6e2e7c57b93b272509cb7a7843031fc56810a15c
Krishna <125199715+krishnasinghmahar@users.noreply.github.com>
2024-09-07 21:31:49 +0530

    Update 02-clock.js
    correct the 12 am and 12 pm bug and change clock to 12 hour clock

--- week-2/01-async-js/medium/02-clock.js
+++ week-2/01-async-js/medium/02-clock.js
@@ -1,10 +1,20 @@
 function clock() {
-    setInterval(() => {
-        let date = new Date();
-        let hour = date.getHours();
-        // HH:MM::SS AM/PM
-        console.log(hour + ":" + date.getMinutes() + ":" + date.getSeconds(), hour > 12 ? "PM" : "AM");
-    }, 1000)
-}
+  setInterval(() => {
+    let date = new Date();
+    let hour = date.getHours();
+    // HH:MM::SS AM/PM
 
+    let isAfternoon = 'AM'
+    if (hour >= 12) {
+      isAfternoon = 'PM'
+      hour = hour - 12
+    }
+
+    if (hour == 0) {
+      hour = 12
+    }
+    
+    console.log(hour + ":" + date.getMinutes() + ":" + date.getSeconds(), isAfternoon);
+  }, 1000)
+}
 clock()
